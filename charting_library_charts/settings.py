871fd0cd7ce6d2c44d55381f3b60f67f48c0ed8b
yashashri0809 <yashashrikavathekar2002@gmail.com>
2024-10-17 18:46:04 +0530

    Update settings.py

--- charting_library_charts/settings.py
+++ charting_library_charts/settings.py
@@ -16,9 +16,9 @@ MANAGERS = ADMINS
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': os.getenv('DB_NAME', 'charting_library'),
+		'NAME': os.getenv('DB_NAME', 'save_load'),
 		'USER': os.getenv('DB_USER', 'postgres'),
-		'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+		'PASSWORD': os.getenv('DB_PASSWORD', '123'),
 		'HOST': os.getenv('DB_HOST', 'localhost'),
 		'PORT': int(os.getenv('DB_PORT', '5432')),
 	}
